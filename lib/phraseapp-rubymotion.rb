aa0100dba569929590ea4ab617cb6d11a14baf94
kevvvvv <kevin@fileperms.org>
2015-06-18 15:04:11 +0200

    remove rake jobs

--- lib/phraseapp-rubymotion.rb
+++ lib/phraseapp-rubymotion.rb
@@ -17,25 +17,3 @@ Motion::Project::App.setup do |app|
     pod 'AFNetworking', '>= 2.5.0'
   end
 end
-
-namespace :phrase do
-  desc "Initialize PhraseApp for your RubyMotion project"
-  task :init do
-    access_token = ENV['AUTH_TOKEN']
-    App.fail "Please specify your auth token, e.g. rake phrase:init AUTH_TOKEN=secret" unless access_token
-    result = `phrase init --secret=#{access_token}`
-    App.info "PHRASE", result
-  end
-
-  desc "Push your Localizable.strings files to PhraseApp"
-  task :push do
-    result = `phrase push --recursive ./resources`
-    App.info "PHRASE", result
-  end
-
-  desc "Pull your current localization files files from PhraseApp"
-  task :pull do
-    result = `phrase pull --target=resources --format=strings`
-    App.info "PHRASE", result
-  end
-end
